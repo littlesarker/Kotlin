b2d649ecb73e07a262199b4caa35f2381efd6189
Md Rahim Sarker <61264159+littlesarker@users.noreply.github.com>
2023-02-13 19:13:30 +0600

    Kotlin  Variables

---
+++ Variables/src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main(args: Array<String>) {
+
+    //variables are containers for data values
+    //in kotlin variable can be (var)/(val)
+
+    var name = "Rahim"
+    var rahim =24
+
+    println(name)
+    println(rahim)
+
+    //The difference between var and val is
+   // that variables declared with the var keyword can be changed/modified, while val variables cannot.
+  //in Kotlin do not need to be declared with a specified type .But if you want you can do that.
+
+    var Uvnam :String="GUB"
+    val age :Int =22
+
+    println(Uvnam)
+    println(age)
+
+
+    //without specify the type of variable .its can"t be un assigned variable
+
+
+}
