5910089663f377c5888d0472e4b57724e4174eb6
Md Rahim Sarker <61264159+littlesarker@users.noreply.github.com>
2023-02-13 19:18:49 +0600

    Data_types

---
+++ Data_types/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+
+     val num: Int =23
+     val RaDouble:Double =2.80
+     val Rletter:Char ='D'
+    val word:String ="Rahim Sarker"
+    val SaBoolean: Boolean=true
+
+    println(num)
+    println(RaDouble)
+    println(Rletter)
+    println(word)
+    println(SaBoolean)
+
+
+}
